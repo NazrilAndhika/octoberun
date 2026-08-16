49f5aeda527d52cbb39f9f97067c601ffb3844d1
NazrilAndhika <nazrilandhika2004@gmail.com>
2026-08-16 17:33:32 +0700

    Fix: Mencegah icon lokasi mengkerut di tampilan mobile

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -139,7 +139,8 @@
 
                 <!-- Stat 4: Lokasi -->
                 <div data-aos="zoom-in" data-aos-delay="400" class="flex items-center justify-center gap-4 px-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-white opacity-90" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
+                    <!-- Tambahkan shrink-0 dan style="flex-shrink: 0;" di sini -->
+                    <svg style="flex-shrink: 0;" xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-white opacity-90 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                     </svg>
